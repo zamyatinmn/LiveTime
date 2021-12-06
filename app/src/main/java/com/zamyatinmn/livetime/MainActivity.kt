1a673be0bebdf9ae09bb10b06a5d6e95f9fd9d67
zamyatinmn <zamyatinmn@gmail.com>
2021-12-06 21:08:43 +0300

    #8 added view binding added abstract ViewBindingFragment

--- app/src/main/java/com/zamyatinmn/livetime/MainActivity.kt
+++ app/src/main/java/com/zamyatinmn/livetime/MainActivity.kt
@@ -2,11 +2,18 @@ package com.zamyatinmn.livetime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zamyatinmn.livetime.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.container, WelcomeFragment.newInstance()).commit()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction()
+            .replace(binding.container.id, WelcomeFragment.newInstance())
+            .commit()
     }
 }
--- app/src/main/java/com/zamyatinmn/livetime/WelcomeFragment.kt
+++ app/src/main/java/com/zamyatinmn/livetime/WelcomeFragment.kt
@@ -1,23 +1,13 @@
 package com.zamyatinmn.livetime
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.zamyatinmn.livetime.databinding.FragmentWelcomeBinding
 
 
-class WelcomeFragment: Fragment() {
+class WelcomeFragment:
+    ViewBindingFragment<FragmentWelcomeBinding>(FragmentWelcomeBinding::inflate) {
 
     companion object {
         fun newInstance() = WelcomeFragment()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
-    }
 }
